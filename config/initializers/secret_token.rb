e736a17336e94b72ea7c8e5d1f17463a2a8448fb
Cristiano Sinadino <cristiano_sinadino@yahoo.com>
2014-03-12 23:19:23 -0700

    generate welcome ,routes and view

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sinadino::Application.config.secret_token = '0e6e13fa3f789c1aa4ebab154adaa4190df217c0f39114cb1f8986d8600a0da239373deddfe89b6b036b66a7ffe97f8d1bfa36d5b2427967ecb23ebfcaabc395'
